1fb24050827bc3f7f57e9801825005816d23067b
Loonixas <loonixas@gmail.com>
2019-05-06 21:03:51 +0300

    Enter Your Name App (Works perfect)

---
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private TextView mTextView;
+    private EditText mEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.mButton);
+        mButton.setText(R.string.do_not_press);
+        mTextView = findViewById(R.id.mTextView);
+        mEditText = findViewById(R.id.editText);
+
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String enteredText;
+
+                enteredText = mEditText.getText().toString();
+
+                mTextView.setVisibility(View.VISIBLE);
+                mTextView.setText(enteredText);
+            }
+        });
+
+    }
+
+
+//    public void showMe(View view){
+//        mTextView.setVisibility(View.VISIBLE);
+//        mTextView.setText(R.string.show_text);
+//    }
+}
